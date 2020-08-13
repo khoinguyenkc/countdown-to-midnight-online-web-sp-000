e21613da2e7f0c6582fbeb37496311ebcc40eacd
Vietkhoi Nguyen <khoinguyenkc@gmail.com>
2020-08-13 21:09:14 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -3,9 +3,12 @@
 def countdown(integer)
   while integer > 0
     puts "#{integer} SECOND(S)!"
+    sleep(1)
     integer -= 1
   end
   "HAPPY NEW YEAR!"
 end
 
 puts countdown(3)
+
+def countdown_with_sleep(integer)
